e9431ecdbfc86ca4bf0188cc15c311ddf518eeb3
Nick Partridge <nicholas.partridge@elastic.co>
2025-04-12 15:03:21 -0500

    add updateMode input

--- playwright.config.ts
+++ playwright.config.ts
@@ -59,9 +59,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
+  webServer: process.env.CI ? {
     command: 'npm start',
     url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
-  },
+  } : undefined,
 });
